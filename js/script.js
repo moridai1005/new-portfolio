447173eaa62e42702784504bc3153b3cdfb0192c
moridai1005 <49890145+moridai1005@users.noreply.github.com>
2020-03-02 19:09:56 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,96 @@
 $(document).ready(function() {
+    var prog = 0;
     $('#fullpage').fullpage({
         anchors: ["top","profile","skills","works","contact"],
-        menu: '.navbar-nav'
+        menu: '.navbar-nav',
+        navigation:true,
+        verticalCentered:true,
+        scrollOverflow:true,
+        onLeave: function(index, nextIndex, direction) {
+            var index = index.index
+            if(index === 0 && direction == 'down') {
+                $(".empty-box").addClass("active1");
+                $(".profile-content").addClass("active2");
+            }
+            if(index===2 && direction == "up"){
+                $(".empty-box").addClass("active1");
+                $(".profile-content").addClass("active2");
+            }
+            if(index===1 && (direction=="up" || direction=="down")){
+                $(".empty-box").removeClass("active1");
+                $(".profile-content").removeClass("active2");
+            }
+        }
     });
-   });
+    
+    window.onload = function() {
+        const spinner = document.getElementById('loading');
+        spinner.classList.add('loaded');
+    }
+
+
+    var prog_plus = function() {
+        if(prog == 400){
+            prog=400;
+        }else {
+        prog += 80;
+        }
+        $('.progress-bar').css({'width':prog});
+        var perc = 100 * prog / 400;
+        $('.progress-bar').text(perc+"%");
+    }
+    setInterval(prog_plus,750);
+
+    $('.mask > .caption').append(
+        '<div class="only-sp"><button class="btn btn-sm btn-secondary close-btn"><span class="fa fa-times"></span></button></div>'
+        );
+    
+    $(".close-btn").click(
+        function(e) {
+            if($(".mask").is(":visible")){
+            console.log("close true");
+            e.stopPropagation();
+            $(this).parents(".mask").hide();
+            }
+        }
+    );
+
+    $(document).click(function(event){
+        console.log("click on");
+        event.stopPropagation();
+        var count = 0;
+        var styles = document.getElementsByClassName("mask");
+        for(var i = 0;i < styles.length;i++){
+            sty = styles[i].style.display;
+            if(sty=="block"){
+                count += 1;
+            }
+            console.log("count="+count);
+        }    
+        if(count==1){
+            if(!$(event.target).closest(".mask").length) {
+
+                console.log("close other true");
+                $(".mask").hide();
+            }
+        }
+    });
+
+
+    $(".detail").click(function() {
+        console.log("detail true");
+        var count = 0;
+        var styles = document.getElementsByClassName("mask");
+        for(var i = 0;i < styles.length;i++){
+            sty = styles[i].style.display;
+            if(sty=="block"){
+                count += 1;
+            }
+            //console.log(count);
+        }    
+        if(count==0) {
+            $(this).find(".mask").fadeIn();
+        }
+    })
+
+});
